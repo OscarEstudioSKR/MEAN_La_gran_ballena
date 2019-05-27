9bc9fd279eaa8d0443f7306fa3dd583f48ee56a2
Skr <oscaraparisisancho@gmail.com>
2019-05-28 00:29:06 +0200

    get user with or without id

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,7 +1,24 @@
 const usersController = {};
+const usersSchema = require('../models/usersSchema');
 
-usersController.getUsers = (req, res) => {
-    res.send('Hello world 2');
+usersController.getUsers = async (req, res) => {
+    const employees = await usersSchema.find();
+    res.json(employees);
 };
 
+usersController.getUser = async (req, res) => {
+    res.json( await usersSchema.findById(req.params.id) );
+};
+
+usersController.postUser = async (req, res) => {
+    const employee = await new usersSchema(req.body);
+    await employee.save();
+    res.json(employee);
+};
+usersController.putUser = function (){
+
+};
+usersController.deleteUser = function (){
+
+};
 module.exports = usersController;
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,5 +3,9 @@ const router = Router();
 const usersController = require('../controllers/usersController');
 
 router.get('/', usersController.getUsers);
+router.get('/:id', usersController.getUser);
+router.delete('/:id', usersController.deleteUser);
+router.post('/', usersController.postUser);
+router.put('/:id', usersController.putUser);
 
 module.exports = router;
