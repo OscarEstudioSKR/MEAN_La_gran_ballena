7b5b2333db68b65bfe6db80624e0df946242e087
Skr <oscaraparisisancho@gmail.com>
2019-05-28 19:26:07 +0200

    Service methods get, put, post and delete from usersService

--- front-end/src/app/services/users.service.ts
+++ front-end/src/app/services/users.service.ts
@@ -5,12 +5,27 @@ import { UsersModel } from '../models/UsersModel';
 @Injectable({
   providedIn: 'root'
 })
+
 export class UsersService {
-  prueba: string = "SKR!!!";
+
   readonly URI_API = "http://localhost:3001/api/";
   selectedUser: UsersModel;
+
   constructor( private http: HttpClient) { }
-  getUser(){
+
+  getUsers(){
     return this.http.get( this.URI_API );
-  }
+  };
+  getUser( _id:string ){
+    return this.http.get( this.URI_API + `/${_id}`);
+  };
+  postUser( user:UsersModel ){
+    return this.http.post( this.URI_API, user);
+  };
+  puttUser( user:UsersModel ){
+    return this.http.put( this.URI_API + `/${user._id}`, user);
+  };
+  deletetUser( _id:string ){
+    return this.http.delete( this.URI_API + `/${_id}`);
+  };
 }
